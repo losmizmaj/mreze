3cc28fd5ddc72ea60e4fd3414f7a672da0fbc5b3
losmizmaj <milossegedi23@gmail.com>
2020-04-15 21:27:55 +0200

    introduce with HTML and change Django view

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 
@@ -21,3 +22,5 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+
